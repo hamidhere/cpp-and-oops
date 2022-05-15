4ed090e1c58a40f9ac676f5267f6a06e04ee0af3
hamidhere <hamiddot47@icloud.com>
2022-05-15 20:52:59 +0500

    a bit errors

--- lec28.cpp
+++ lec28.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
+class X
+{
+    int data;
+friend void add(); 
+public:
+    void setValue(int value)
+    {
+        data = value;
+    }
+};
+class Y
+{
+    int num;
+friend void add();
+public:
+    void setValue(int value)
+    {
+        num = value;
+    }
+};
+void add(X o1, Y o2){
+    cout<<"Sum of the data is"<<o1.data+o2.num<<endl;
+}
 
 int main()
 {
+    X a;
+    a.setValue(15);
+
+    Y b;
+    b.setValue(5);
+
 
     return 0;
 }
