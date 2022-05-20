068edb75c7b6266febb7801c0f58082ac642a582
hamidhere <hamiddot47@icloud.com>
2022-05-20 11:52:45 +0500

    Constructers with example

--- lec29.cpp
+++ lec29.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 using namespace std;
+class Complex
+{
+    int a, b;
+
+public:
+    Complex(void);
+    void printNumber()
+    {
+        cout << "Sum of complex numbers is: " << a << " + " << b << "i" << endl;
+    }
+};
+Complex ::Complex()
+{
+    a = 10;
+    b = 20;
+}
 
 int main()
 {
+    Complex c1, c2, c3;
+    c1.printNumber();
+    c2.printNumber();
+    c3.printNumber();
 
     return 0;
 }
