f0295bc02975e4e161fca9153987186cbf736f1b
hamidhere <hamiddot47@icloud.com>
2022-05-25 21:26:46 +0500

    call byt reference &call by value

--- lec16.cpp
+++ lec16.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
-
-int sum(int a,int b)
+// call byt reference &call by value
+int sum(int a, int b)
 {
-int c=a+b;
-return c;
+    int c = a + b;
+    return c;
 };
 /*void swapPtr(int* a,int* b){ //call byt reference using pointers
     int temp=*a;
         *a=*b;
         *b=temp;
-                
+
 };*/
-void swapRefVar(int& a,int& b){ //call by reference using CPP variable.
-    int loc=a;
-        a=b;
-        b=loc; 
-    
+void swapRefVar(int &a, int &b)
+{ // call by reference using CPP variable.
+    int loc = a;
+    a = b;
+    b = loc;
 };
 
-
-    int main(){         
-    int x=4,y=5;
-    cout<<"The value of x is: "<<x<<".  The value of y is: "<<y<<endl;
-    swapRefVar(x,y);
-    cout<<"The value of x is: "<<x<<".  The value of y is: "<<y<<endl;
-        //cout<<"the sum of 4 and 5 is: "<<sum(a,b)<<endl;     
+int main()
+{
+    int x = 4, y = 5;
+    cout << "The value of x is: " << x << ".  The value of y is: " << y << endl;
+    swapRefVar(x, y);
+    cout << "The value of x is: " << x << ".  The value of y is: " << y << endl;
+    // cout<<"the sum of 4 and 5 is: "<<sum(a,b)<<endl;
 
     return 0;
 }
