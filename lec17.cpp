bdcf6f9d24fba4e3ea5c6472fd4c7afbc963e62f
hamidhere <hamiddot47@icloud.com>
2022-05-25 21:27:21 +0500

    inline functions,default arguments,constant arguments

--- lec17.cpp
+++ lec17.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-inline int product(int x,int y){
-    // in inline fucntion compiler places the code of function into main function's output call 
-    static int c=0;
-    c=c+1;
-    return x*y+c;
+inline int product(int x, int y)
+{
+    // in inline fucntion compiler places the code of function into main function's output call
+    static int c = 0;
+    c = c + 1;
+    return x * y + c;
 }
-int main(){
+int main()
+{
     //****inline functions,default arguments,constant arguments
-    int a,b;
-    cout<<"enter 2 numbers a and b"<<endl;
-    cin>>a>>b;
-    cout<<"product of the a and b  numbers is: "<<product(a,b)<<endl;
-    cout<<"product of the a and b numbers is: "<<product(a,b)<<endl;
-    cout<<"product of the a and b numbers is: "<<product(a,b)<<endl;
-
-
+    int a, b;
+    cout << "enter 2 numbers a and b" << endl;
+    cin >> a >> b;
+    cout << "product of the a and b  numbers is: " << product(a, b) << endl;
+    cout << "product of the a and b numbers is: " << product(a, b) << endl;
+    cout << "product of the a and b numbers is: " << product(a, b) << endl;
 
     return 0;
 }
