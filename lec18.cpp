3946830b36478232e5436ad7dd6ca2e83df9b726
hamidhere <hamiddot47@icloud.com>
2022-05-25 22:02:17 +0500

    Recusions and recursive functions

--- lec18.cpp
+++ lec18.cpp
@@ -5,25 +5,29 @@ using namespace std;
     //recursive functions-->Calling the same function within that function.
     if (n<=1){
     return 1;
-}    
+}
     return n*fectorial(n-1);
 
 }*/
-int fib(int n){
-    if (n<2){
+int fib(int n)
+{
+    if (n < 2)
+    {
         return n;
     }
-    return fib(n-2) +(n-1);
+    return fib(n - 2) + (n - 1);
 }
-int main(){
-    
-    int a,i=0;
-    cout<<"Enter the number"<<endl;
-    cin>>a;
-    cout<<"Febonaci expression of "<<a<<" is "<<fib(a)<<endl;
-    while(i<a){
-    cout<<"Febonaci expression of "<<i<<" is "<<fib(i)<<endl;
-    i++;
+int main()
+{
+
+    int a, i = 0;
+    cout << "Enter the number" << endl;
+    cin >> a;
+    cout << "Febonaci expression of " << a << " is " << fib(a) << endl;
+    while (i < a)
+    {
+        cout << "Febonaci expression of " << i << " is " << fib(i) << endl;
+        i++;
     }
     return 0;
 }
