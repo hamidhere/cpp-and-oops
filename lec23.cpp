6f25149826eb15ef37b18a0b4307d53f4a69da2e
hamidhere <hamiddot47@icloud.com>
2022-05-25 22:06:38 +0500

    Memory allocation and using arrays in classes

--- lec23.cpp
+++ lec23.cpp
@@ -14,21 +14,18 @@ public:
 };
 void shop ::setPrice(void)
 {
-    cout<<"Enter Id of your item:"<<counter+1<<endl;
-    cin>>itemId[counter];
-    cout<<"Set price of your item:"<<itemId[counter]<<endl;
-    cin>>itemPrice[counter];
+    cout << "Enter Id of your item:" << counter + 1 << endl;
+    cin >> itemId[counter];
+    cout << "Set price of your item:" << itemId[counter] << endl;
+    cin >> itemPrice[counter];
     counter++;
-   
 }
 void shop::displayprice(void)
 {
     for (int i = 0; i < counter; i++)
     {
-        cout<<"The price for your item "<<itemId[i]<<" is: "<<itemPrice[i]<<endl;
+        cout << "The price for your item " << itemId[i] << " is: " << itemPrice[i] << endl;
     }
-    
-
 }
 
 int main()
@@ -40,7 +37,6 @@ int main()
     mart.setPrice();
     mart.setPrice();
     mart.displayprice();
-
 
     return 0;
 }
