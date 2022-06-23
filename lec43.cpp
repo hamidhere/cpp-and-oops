e838c96f4503ca045e8b5b49c47f9e1bce2ab200
hamidhere <hamiddot47@icloud.com>
2022-06-23 14:48:00 +0500

    Ambiguity in Inheritance

--- lec43.cpp
+++ lec43.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 using namespace std;
-//ambiguity in inheritance
+// ambiguity in inheritance
+class Base1
+{
+public:
+    void Greet()
+    {
+        cout << "hello" << endl;
+    }
+};
+class Base2
+{
+public:
+    void Greet()
+    {
+        
+        cout << "Suna ki hall ny" << endl;
+    }
+};
+class Derived : public Base1, public Base2
+{
+    public:
+    void Greet()
+    {
+       Base1 :: Greet();
 
-int main(){
-
-    
-
-
+    }
+};
+int main()
+{
+    Base1 obj1;
+    obj1.Greet();
+    Base2 obj2;
+    obj2.Greet();
+    Derived obj3;;
+    obj3.Greet();
 
     return 0;
 }
