a22434bec46af53a57379b29ba897d6fd9f8469e
hamidhere <hamiddot47@icloud.com>
2022-07-07 18:44:53 +0500

    Mapping

---
+++ lec73.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<string,string> strN;
+    strN["hamid"]="raza";
+    map<string, string>::iterator itr;
+    itr=strN.begin();
+    cout<<(*itr).first<<" "<< (*itr).second<<endl;
+    itr=strN.end();
+
+        return 0;
+}
